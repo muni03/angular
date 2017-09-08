09d45cce7177a870fb039b770cee3cc79f7708fd
Peter Muniz <pmuniz03@gmail.com>
2017-09-08 05:44:48 -0500

    Recipe app start!

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -6,10 +6,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  userCreationStatus = 'No user was created!';
+  serverName ='Testserver';
+  userName = '';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
+  showSecret = false;
+  log = [];
 
-  constructor() { }
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
 
   ngOnInit() {
   }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onCreateUser() {
+    this.userCreationStatus = 'User was created! Name is ' + this.userName;
+    this.userName = '';
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = event.target.value;
+  }
+
+  onUpdateUserName(event: any) {
+    this.userName = event.target.value;
+  }
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    //this.log.push(this.log.length + 1);
+    this.log.push(new Date());
+  }
+
 
 }
